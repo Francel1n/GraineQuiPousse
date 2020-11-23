a3b7c216666b37e9ab90d3d23fe25eb224fbc7f4
aliceKatkout <alice.katkout@gmail.com>
2020-11-23 17:15:45 +0100

    blou

--- animation.js
+++ animation.js
@@ -8,7 +8,20 @@ let totalimgNumber = 6;
 let anim = [];
 
 for (i=0; i<totalimgNumber; i++){
-  anim[i]= new Image (2000, 815);
+  anim[i]= new Image (1000, 815);
   anim[i].src ="imagesSVG/graine"+ (i+1)+ ".svg";
 }
-document.graine.src = anim[1].src;
+
+function switch2() {
+document.graine.src = anim[imgNumber].src;
+  imgNumber++;
+  if (imgNumber >= totalimgNumber) {
+   imgNumber = 0;
+ }
+}
+
+function animate1() {
+  setInterval(switch2, 1000);
+}
+
+onLoad= animate1()
