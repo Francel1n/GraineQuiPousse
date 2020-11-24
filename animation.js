dca921013ca84a07f74f825fb4a6e564f3132793
aliceKatkout <alice.katkout@gmail.com>
2020-11-24 15:50:12 +0100

    vivus fonctionnel

--- animation.js
+++ animation.js
@@ -3,25 +3,32 @@ const millisecondes_connexion = Date.now();
 document.getElementById("p1").innerHTML = date;
 document.getElementById("p2").innerHTML = millisecondes_connexion;
 
-let imgNumber = 0;
-let totalimgNumber = 6;
-let anim = [];
+AnimFleur = new Vivus("fleur",{
+    type: 'oneByOne',
+    duration: 10000,
+});
 
-for (i=0; i<totalimgNumber; i++){
-  anim[i]= new Image (1000, 815);
-  anim[i].src ="imagesSVG/graine"+ (i+1)+ ".svg";
-}
+//document.getElementById("graine").innerHTML = AnimFleur;
 
-function switch2() {
-document.graine.src = anim[imgNumber].src;
-  imgNumber++;
-  if (imgNumber >= totalimgNumber) {
-   imgNumber = 0;
- }
-}
-
-function animate1() {
-  setInterval(switch2, 1000);
-}
-
-onLoad= animate1()
+// let imgNumber = 0;
+// let totalimgNumber = 6;
+// let anim = [];
+//
+// for (i=0; i<totalimgNumber; i++){
+//   anim[i]= new Image (1000, 815);
+//   anim[i].src ="imagesSVG/graine"+ (i+1)+ ".svg";
+// }
+//
+// function switch2() {
+// document.graine.src = anim[imgNumber].src;
+//   imgNumber++;
+//   if (imgNumber >= totalimgNumber) {
+//    imgNumber = 0;
+//  }
+// }
+//
+// function animate1() {
+//   setInterval(switch2, 1000);
+// }
+//
+// onLoad= animate1()
